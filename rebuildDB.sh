12c7dbd2d129fac018c4e0a70e3d7ab2cfe11c53
Gowtham2543 <gowtham.manoharan@outlook.com>
2023-04-06 09:55:52 +0530

    RebuildDB.sh can rut outside virtualenv

--- rebuildDB.sh
+++ rebuildDB.sh
@@ -2,6 +2,6 @@
 
 # Run this file if any modifications is done to the models
 
-python3 dropDB.py #Drops all the tables in the DB
+pipenv run python3 dropDB.py #Drops all the tables in the DB
 
-python3 createDB.py #Creates all the schemas
+pipenv run python3 createDB.py #Creates all the schemas
