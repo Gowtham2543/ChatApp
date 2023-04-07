2a3d380d60a27a904992f139c447b4b4d2e0f780
Gowtham2543 <gowtham.manoharan@outlook.com>
2023-04-07 13:03:07 +0530

    Combined dropDB and createDB into one file

--- dropDB.py
+++
@@ -1,5 +0,0 @@
-from api.models import db
-from api.app import app
-
-with app.app_context():
-    db.drop_all()
--- rebuildDB.sh
+++ rebuildDB.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
-export PIPENV_VERBOSITY=-1
 # Run this file if any modifications is done to the models
 
-pipenv run python3 dropDB.py #Drops all the tables in the DB
+#!/bin/bash
+export PIPENV_VERBOSITY=-1 # Ro supress stderr when the below command is run inside pipenv shell
 
-pipenv run python3 createDB.py #Creates all the schemas
+pipenv run python3 ./api/rebuildDB.py 
+
