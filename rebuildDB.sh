1c066b0ad9415082fe21c0d9603d95a363bc0f3c
Gowtham2543 <gowtham.manoharan@outlook.com>
2023-07-02 12:58:54 +0530

    Refactoring

--- rebuildDB.sh
+++ rebuildDB.sh
@@ -1,7 +1,7 @@
 # Run this file if any modifications is done to the models
 
 #!/bin/bash
-export PIPENV_VERBOSITY=-1 # Ro supress stderr when the below command is run inside pipenv shell
+export PIPENV_VERBOSITY=-1 # To supress stderr when the below command is run inside pipenv shell
 
 pipenv run python3 ./rebuildDB.py 
 
